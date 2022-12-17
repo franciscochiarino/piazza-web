9c4cbafae8dcad385a234fac435798ed4d66aecf
Fran <francisco.chiarino@sofatutor.com>
2022-12-17 20:18:39 -0300

    has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
 
   before_validation :strip_spaces
 
   validates :name, presence: true
-  validates :email,
-    format: { with: URI::MailTo::EMAIL_REGEXP },
-    uniqueness: { case_sensitive: false }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
 
   private
 
