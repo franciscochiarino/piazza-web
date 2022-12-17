b86a2fc74b2a11044a6ec65004a0cac9bce216d3
Fran <francisco.chiarino@sofatutor.com>
2022-12-17 20:06:37 -0300

    srip spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
 
+  before_validation :strip_spaces
+
   validates :name, presence: true
   validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: { case_sensitive: false }
+
+  private
+
+    def strip_spaces
+      self.name = self.name&.strip
+      self.email = self.email&.strip
+    end
 end
