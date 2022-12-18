5b3499dbd43a55f0a80659ac1aa8a83c964baee4
Fran <francisco.chiarino@sofatutor.com>
2022-12-18 13:46:30 -0300

    skip authentication and allow authenticated

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -1,7 +1,29 @@
 module Authenticate
   extend ActiveSupport::Concern
 
+  included do
+    before_action :authenticate
+    before_action :require_login, unless: :logged_in?
+
+    helper_method :logged_in?
+  end
+
+  class_methods do
+    def skip_authentication(**options)
+      skip_before_action :authenticate, options
+      skip_before_action :require_login, options
+    end
+
+    def allow_unauthenticated(**options)
+      skip_before_action :require_login, options
+    end
+  end
+
   protected
+
+    def logged_in?
+      Current.user.present?
+    end
 
     def log_in(app_session)
       cookies.encrypted.permanent[:app_session] = {
@@ -10,6 +32,12 @@ module Authenticate
     end
 
   private
+
+    def require_login
+      flash.now[:notice] = t("login_required")
+
+      render("sessions/new", status: unauthorized)
+    end
 
     def authenticate
       Current.app_session = authenticate_using_cookie
@@ -24,8 +52,8 @@ module Authenticate
 
     def authenticate_using(data)
       data => { user_id:, app_session:, token: }
-
       user = User.find(user_id)
+
       user.authenticate_app_session(app_session, token)
     rescue NoMatchingPatternError, ActiveRecord::RecordNotFound
       nil
