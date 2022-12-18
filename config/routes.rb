a17485b19ad49622662d0dd546a730400018e866
Fran <francisco.chiarino@sofatutor.com>
2022-12-17 22:05:47 -0300

    create user and feed controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "feed#show"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "sign_up", to: "users#new"
+  get "sugn_up", to: "users#create"
 end
